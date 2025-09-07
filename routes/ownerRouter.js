56e47f3674e07386901418891a4dac64f86db5ef
JeevanVankadara <jeevanv1997@gmail.com>
2025-09-07 17:25:52 +0530

    added owner changing details

--- routes/ownerRouter.js
+++ routes/ownerRouter.js
@@ -48,11 +48,62 @@ router.get("/index",isOwnerLoggedin,async (req,res)=>{
 
 router.post("/login",loginOwner);
 
+router.get("/logout",isOwnerLoggedin,(req,res)=>{
+    res.cookie("Ownertoken","");
+    res.redirect("/owners/create");
+});
 
-
-router.get("/admin", function(req,res){
+router.get("/myaccount",isOwnerLoggedin,async (req,res)=>{
+    let owner=await ownerModel.findOne({email:req.owner.email});
     let success=req.flash("success");
-    res.render("createproducts",{success});
+    let error=req.flash("error");
+    res.render("ownerpages/myaccount",{owner,success,error});
+});
+
+router.get("/addproduct",isOwnerLoggedin,function(req,res){
+    let success=req.flash("success");
+    let error=req.flash("error")
+    res.render("createproducts",{success,error});
+});
+
+router.post("/changedetails",isOwnerLoggedin,async (req,res)=>{
+    let owner=await ownerModel.findOne({email:req.owner.email});
+    let{fullname,email,oldpass,newpass,confnewpass,contact,gstin}=req.body;
+    if(!owner){
+        req.flash("error","Login first");
+        return res.redirect("/owner/create");
+    }
+
+    bcrypt.compare(oldpass,owner.password,async (err,result)=>{
+        if(result){
+            owner.fullname=fullname;
+            owner.email=email;
+            owner.contact=contact;
+            owner.gstin=gstin;
+            if(newpass && confnewpass){
+                if(newpass===confnewpass){
+                    bcrypt.genSalt(10,(err,salt)=>{
+                        bcrypt.hash(newpass,salt,async (err,hash)=>{
+                            if(err){
+                                req.flash("error","something went wrong");
+                                return res.redirect("/owners/myaccount")
+                            }
+                            owner.password=hash;
+                            await owner.save();
+                        })
+                    })
+                }
+            }
+            await owner.save();
+            req.flash("success","updated successfully");
+            res.redirect("/owners/myaccount");
+        }
+        else{
+            req.flash("error","Old password mismatched");
+            res.redirect("/owners/myaccount");
+        }
+    })
+
 })
 
 module.exports=router;
